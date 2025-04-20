708af8433f454d4c592f466d3690338d77e62b00
dennissammy77 <dennissammy77@gmail.com>
2025-04-20 16:22:11 +0300

    writing testss

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,32 @@
+const { isPalindrome, hasUpperLower, isEmpty, isNotAlphabetic,isNotAString } = require("../utils");
+
 // Your tests here
+describe("palindromes receive double the ponts",()=>{
+    it('checks if a word is a palindrome',()=>{
+        // arrange
+        const text = 'mom'
+        // act
+        const checker = isPalindrome(text);
+        // assert
+        expect(checker).toBe(true)
+    });
+    it("Is a combination of upper and lowercase",()=>{
+        const text = 'Mom';
+        const checker = hasUpperLower(text);
+        expect(checker).toBe(true)
+    });
+    it("Is empty",()=>{
+        const text = '';
+        const checker = isEmpty(text);
+        expect(checker).toBe(true)
+    })
+    it("does not have alphabetic letters",()=>{
+        const text = '112sawaw';
+        expect(()=>isNotAlphabetic(text)).toThrow('Does not have any alphabetic letters')
+    })
+    it("is not a string",()=>{
+        const text = '11212';
+        expect(()=>isNotAString(text)).toThrow('Is not a string')
+    })
+
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,31 @@
 // Your code here
+export function isPalindrome(text){
+    if(text.split('').reverse().join('') === text){
+        return true;
+    }
+    return false;
+}
+export function hasUpperLower(text){
+    if(/[A-Za-z]/.test(text)){
+        return true;
+    }
+    return false;
+};
+export function isEmpty(text){
+    if(text.length === 0){
+        return true
+    }
+    return false;
+};
+export function isNotAlphabetic(text){
+    if(!/^[A-Za-z]+$/.test(text)){
+        throw new Error('Does not have any alphabetic letters');
+    }
+    return text
+};
+export function isNotAString(text){
+    if(!/^[A-Za-z]+$/.test(text)){
+        throw new Error('Is not a string');
+    }
+    return text
+};
